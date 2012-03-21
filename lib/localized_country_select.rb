9455e6a5c85361e5ab61ed060dd221867ed9eab0
John Agan <john@assistly.com>
2012-03-21 10:52:01 -0400

    Latest build of rails wouldn't render the option tags

--- lib/localized_country_select.rb
+++ lib/localized_country_select.rb
@@ -56,7 +56,7 @@ module ActionView
       # It behaves likes older object-binded brother +localized_country_select+ otherwise
       # TODO : Implement pseudo-named args with a hash, not the "somebody said PHP?" multiple args sillines
       def localized_country_select_tag(name, selected_value = nil, priority_countries = nil, html_options = {})
-        select_tag name.to_sym, localized_country_options_for_select(selected_value, priority_countries), html_options.stringify_keys
+        select_tag name.to_sym, localized_country_options_for_select(selected_value, priority_countries).html_safe, html_options.stringify_keys
       end
 
       # Returns a string of option tags for countries according to locale. Supply the country code in upper-case ('US', 'DE') 
@@ -82,7 +82,7 @@ module ActionView
         value = value(object)
         content_tag("select",
           add_options(
-            localized_country_options_for_select(value, priority_countries),
+            localized_country_options_for_select(value, priority_countries).html_safe,
             options, value
           ), html_options
         )
